ddf916b7697b9480a97e60d8294c4fc25f548445
Edward <64510369+edwardough@users.noreply.github.com>
2022-02-01 13:44:41 +1100

    Add files via upload

---
+++ Summer21-22/script.js
@@ -0,0 +1,5 @@
+var daysoftheweek = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+var date = new Date();
+//var theDayIndex = date.getDay();
+//console.log(theDayIndex);
+alert("Thank you for visiting my 2021-22 Summer. The date is: " + date);
